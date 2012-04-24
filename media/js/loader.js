02de5640a61c9f6cec6c42c1a067217c8bbe6488
Michal Čihař <mcihar@suse.cz>
2012-04-24 15:10:58 +0200

    Add Français

--- media/js/loader.js
+++ media/js/loader.js
@@ -21,6 +21,7 @@ $(function() {
         $('<ul>' +
             '<li><a href="index.html">English</a></li>' +
             '<li><a href="index.cs.html">Česky</a></li>' +
+            '<li><a href="index.fr.html">Français</a></li>' +
             '<li><a href="index.tr.html">Türkçe</a></li>' +
             '<li><a href="index.pl.html">Polski</a></li>' +
             '</ul>').dialog({title: 'Choose language'});
