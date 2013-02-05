367965ec6f3223810bc8b39235b2eb61aea97aaa
Michal Čihař <mcihar@suse.cz>
2013-02-05 16:57:17 +0100

    Coding style and headers

--- weblate_web/templatetags/downloads.py
+++ weblate_web/templatetags/downloads.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2012 - 2013 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate <http://weblate.org/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 from django.template import Library, Node, Variable, TemplateSyntaxError
 import os
 from django.utils.translation import ugettext as _, ungettext
@@ -5,6 +25,7 @@ from django.utils.safestring import mark_safe
 from django.conf import settings
 
 register = Library()
+
 
 def filesizeformat(bytes):
     """
@@ -23,6 +44,7 @@ def filesizeformat(bytes):
     if bytes < 1024 * 1024 * 1024:
         return _("%.1f MiB") % (bytes / (1024 * 1024))
     return _("%.1f GiB") % (bytes / (1024 * 1024 * 1024))
+
 
 @register.simple_tag
 def downloadlink(name, text = None):
