648cbaa51b9bf82199b5826740b891687a891f79
Michal Čihař <mcihar@suse.cz>
2013-02-07 15:09:15 +0100

    Missing import

--- weblate_web/context_processors.py
+++ weblate_web/context_processors.py
@@ -18,7 +18,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from weblate_web.data import VERSION, EXTENSIONS
+from weblate_web.data import VERSION, EXTENSIONS, SCREENSHOTS
 
 
 def weblate_web(request):
