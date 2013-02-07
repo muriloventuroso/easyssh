15c2d9ce282956ecb3421241900d5d21ea025206
Michal Čihař <mcihar@suse.cz>
2013-02-07 16:13:27 +0100

    Add colorbox for images navigation

--- weblate_web/media/code.js
+++ weblate_web/media/code.js
@@ -2,4 +2,12 @@ $(function() {
     $('#languages').change(function () {
         window.location.href = $(this).val();
     });
+    $('a.screenshot').colorbox({
+        rel:'gal',
+        maxWidth:'100%',
+        maxHeight:'100%',
+        width:'100%',
+        height:'100%',
+        current: '{current}/{total}',
+    });
 });
