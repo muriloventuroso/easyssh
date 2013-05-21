c5b138fcb2e4fb1c5133fde086f9a23308e1b17d
Michal Čihař <mcihar@suse.cz>
2013-05-21 10:59:24 +0200

    Fix spacing

--- weblate_web/media/code.js
+++ weblate_web/media/code.js
@@ -1,13 +1,13 @@
-$(function() {
+$(function () {
     $('#languages').change(function () {
         window.location.href = $(this).val();
     });
     $('.screenshot').colorbox({
-        rel:'gal',
-        maxWidth:'100%',
-        maxHeight:'100%',
-        width:'100%',
-        height:'100%',
+        rel: 'gal',
+        maxWidth: '100%',
+        maxHeight: '100%',
+        width: '100%',
+        height: '100%',
         current: '{current}/{total}',
     });
 });
