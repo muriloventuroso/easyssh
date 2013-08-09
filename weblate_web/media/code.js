0b107a874774a95dc2889f5c9cd7d97de3e2288a
Michal Čihař <mcihar@suse.cz>
2013-08-09 15:20:16 +0200

    Make clicking on button work

--- weblate_web/media/code.js
+++ weblate_web/media/code.js
@@ -11,8 +11,11 @@ $(function () {
     });
     $(document).mouseup(function (e) {
         var languages = $('.languages');
+        var language = $('.language');
 
-        if (!languages.is(e.target) && languages.has(e.target).length === 0) {
+        if (language.is(e.target) || language.has(e.target).length > 0) {
+            return;
+        } else if (!languages.is(e.target) && languages.has(e.target).length === 0) {
             languages.hide();
         }
     });
