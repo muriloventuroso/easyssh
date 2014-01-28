49f98b530ba1150ce364ebbf0b431b9b2bcbcf90
Michal Čihař <mcihar@suse.cz>
2014-01-28 10:44:36 +0100

    Drop secret key completely

--- weblate_web/settings.py
+++ weblate_web/settings.py
@@ -145,9 +145,6 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = ''
-
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
