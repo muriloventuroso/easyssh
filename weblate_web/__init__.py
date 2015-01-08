0e67b9efc663efd88ab2b68e6763847cfdd9f134
Michal Čihař <michal@cihar.com>
2015-01-08 11:13:25 +0100

    Another workaround for Django locale loader
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate_web/__init__.py
+++ weblate_web/__init__.py
@@ -18,14 +18,39 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-import django.utils.translation.trans_real
+import django.utils.translation.trans_real as django_trans
+from copy import _EmptyClass
 import re
 
 # Monkey patch locales, workaround for
 # https://code.djangoproject.com/ticket/24063
-django.utils.translation.trans_real.language_code_re = re.compile(
+django_trans.language_code_re = re.compile(
     r'^[a-z]{1,8}(?:-[a-z0-9]{1,8})*(?:@[a-z0-9]{1,20})?$', re.IGNORECASE
 )
-django.utils.translation.trans_real.language_code_prefix_re = re.compile(
+django_trans.language_code_prefix_re = re.compile(
     r'^/([\w@-]+)(/|$)'
 )
+
+
+class DjangoTranslation(django_trans.DjangoTranslation):
+    """
+    Unshared _info and _catalog to avoid Django messing up
+    locale variants.
+
+    This will not be needed in Django 1.8.
+    """
+    def __copy__(self):
+        """
+        Simplified version of copy._copy_inst extended for copying
+        _info and _catalog.
+        """
+        result = _EmptyClass()
+        result.__class__ = self.__class__
+        state = self.__dict__
+        result.__dict__.update(state)
+        result._info = self._info.copy()
+        result._catalog = self._catalog.copy()
+        return result
+
+
+django_trans.DjangoTranslation = DjangoTranslation
