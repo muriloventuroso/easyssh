5f0f34d2b9dfe6205bd18ef9f7600257b232e56d
Michal Čihař <michal@cihar.com>
2015-02-09 16:39:18 +0100

    Silent false positive
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate_web/wsgi.py
+++ weblate_web/wsgi.py
@@ -40,6 +40,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weblate_web.settings")
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
+# pylint: disable=E402
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
