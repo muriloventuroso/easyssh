e141eda5354f60b2b187b63621e3c5ecc1ed8e32
Michal Čihař <michal@cihar.com>
2015-02-09 16:47:15 +0100

    Cleanup code
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate_web/wsgi.py
+++ weblate_web/wsgi.py
@@ -37,13 +37,9 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weblate_web.settings")
 
+from django.core.wsgi import get_wsgi_application
+
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
-# pylint: disable=E402
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
