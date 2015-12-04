329ba0f743adf03b1b23188e4e6e79d63735de15
Michal Čihař <michal@cihar.com>
2015-12-04 16:13:39 +0100

    Remove unused middleware
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate_web/settings.py
+++ weblate_web/settings.py
@@ -167,8 +167,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
