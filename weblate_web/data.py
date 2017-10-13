a744420998d4ba66f77cddb1970d65b1cc16f307
Michal Čihař <michal@cihar.com>
2017-10-13 17:11:01 +0200

    2.17.1 is out
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate_web/data.py
+++ weblate_web/data.py
@@ -21,7 +21,7 @@
 from django.utils.translation import ugettext_lazy as _
 
 # Version offered for download
-VERSION = '2.16'
+VERSION = '2.17.1'
 
 # Extensions offered for donwload
 EXTENSIONS = ('tar.xz', 'tar.bz2', 'tar.gz')
