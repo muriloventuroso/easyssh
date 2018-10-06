0191a3de554b848c8b5aaea8fa4009783b6a10d0
Michal Čihař <michal@cihar.com>
2018-10-06 14:46:04 +0200

    Weblate 3.2 will be out
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate_web/data.py
+++ weblate_web/data.py
@@ -21,7 +21,7 @@
 from django.utils.translation import ugettext_lazy as _
 
 # Version offered for download
-VERSION = '3.1.1'
+VERSION = '3.2'
 
 # Extensions offered for donwload
 EXTENSIONS = ('tar.xz', 'tar.bz2', 'tar.gz')
